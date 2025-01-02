b41c3c5dba9610a02d7ec1fdabacc2ec867b9a80
Jesus <ivoice666@hotmail.com>
2025-01-02 12:53:02 +0100

    Implemented flag -d (message decoding) for Base64

--- srcs/base64.c
+++ srcs/base64.c
@@ -6,15 +6,15 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 17:53:09 by jesuserr          #+#    #+#             */
-/*   Updated: 2024/12/29 21:01:40 by jesuserr         ###   ########.fr       */
+/*   Updated: 2025/01/02 12:47:32 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "incs/ft_ssl.h"
 
-static void	print_converted_triplet(uint8_t *triplet, int fd, uint8_t scenario)
+static void	print_encoded_triplet(uint8_t *triplet, int fd, uint8_t scenario)
 {
-	uint8_t		output[BASE64_OUTPUT_BLOCKS + 1];
+	uint8_t		output[BASE64_DEC_BLOCKS + 1];
 
 	output[0] = g_base64_table[triplet[0] >> 2];
 	if (scenario == 1)
@@ -41,29 +41,86 @@ static void	print_converted_triplet(uint8_t *triplet, int fd, uint8_t scenario)
 
 static void	encode_message(t_base64_data *base64_data)
 {
-	uint8_t		triplet[BASE64_INPUT_BLOCKS];
+	uint8_t		triplet[BASE64_ENC_BLOCKS];
 	uint64_t	i;
 
 	i = 0;
-	while (i < (base64_data->args->message_length / BASE64_INPUT_BLOCKS) * 3)
+	while (i < (base64_data->args->message_length / BASE64_ENC_BLOCKS) * 3)
 	{
-		ft_memcpy(triplet, base64_data->args->message + i, BASE64_INPUT_BLOCKS);
-		print_converted_triplet(triplet, base64_data->args->output_fd, 1);
-		i += BASE64_INPUT_BLOCKS;
-		if ((i * BASE64_OUTPUT_BLOCKS / BASE64_INPUT_BLOCKS) % BASE64_LINE == 0)
+		ft_memcpy(triplet, base64_data->args->message + i, BASE64_ENC_BLOCKS);
+		print_encoded_triplet(triplet, base64_data->args->output_fd, 1);
+		i += BASE64_ENC_BLOCKS;
+		if ((i * BASE64_DEC_BLOCKS / BASE64_ENC_BLOCKS) % BASE64_LINE == 0)
 			ft_putstr_fd("\n", base64_data->args->output_fd);
 	}
-	if (base64_data->args->message_length % BASE64_INPUT_BLOCKS == 1)
+	if (base64_data->args->message_length % BASE64_ENC_BLOCKS == 1)
 	{
 		ft_memcpy(triplet, base64_data->args->message + i, 1);
-		print_converted_triplet(triplet, base64_data->args->output_fd, 2);
+		print_encoded_triplet(triplet, base64_data->args->output_fd, 2);
 	}
-	else if (base64_data->args->message_length % BASE64_INPUT_BLOCKS == 2)
+	else if (base64_data->args->message_length % BASE64_ENC_BLOCKS == 2)
 	{
 		ft_memcpy(triplet, base64_data->args->message + i, 2);
-		print_converted_triplet(triplet, base64_data->args->output_fd, 3);
+		print_encoded_triplet(triplet, base64_data->args->output_fd, 3);
 	}
 	ft_putstr_fd("\n", base64_data->args->output_fd);
+}
+
+static void	decode_message(t_base64_data *base64_data)
+{
+	uint8_t		quartet[BASE64_DEC_BLOCKS];
+	uint8_t		output[BASE64_ENC_BLOCKS];
+	uint8_t		reverse_table[4];
+	uint64_t	i;
+
+	i = 0;
+	while (i < (base64_data->args->message_length / BASE64_DEC_BLOCKS) * 4)
+	{
+		ft_memcpy(quartet, base64_data->args->message + i, BASE64_DEC_BLOCKS);
+		reverse_table[0] = g_base64_reverse_table[quartet[0]];
+		reverse_table[1] = g_base64_reverse_table[quartet[1]];
+		reverse_table[2] = g_base64_reverse_table[quartet[2]];
+		reverse_table[3] = g_base64_reverse_table[quartet[3]];
+		output[0] = (reverse_table[0] << 2) | (reverse_table[1] >> 4);
+		output[1] = (reverse_table[1] << 4) | (reverse_table[2] >> 2);
+		output[2] = (reverse_table[2] << 6) | reverse_table[3];
+		ft_putchar_fd((char)output[0], base64_data->args->output_fd);
+		if (quartet[2] != '=')
+			ft_putchar_fd((char)output[1], base64_data->args->output_fd);
+		if (quartet[3] != '=')
+			ft_putchar_fd((char)output[2], base64_data->args->output_fd);
+		i += BASE64_DEC_BLOCKS;
+		if (base64_data->args->message[i] == '\n')
+			i++;
+	}
+}
+
+// detect this case echo -n "SG94YQ=o" | ./ft_ssl base64 -d
+static bool	proper_encoded_message(t_base64_data *base64_data)
+{
+	uint64_t	i;
+	uint64_t	message_length_no_newlines;
+	uint64_t	message_length;
+	uint64_t	equals_count;
+
+	message_length = base64_data->args->message_length;
+	message_length_no_newlines = base64_data->args->message_length;
+	equals_count = 0;
+	i = 0;
+	while (i < message_length)
+	{
+		if (base64_data->args->message[i] == '\n')
+			message_length_no_newlines--;
+		if (base64_data->args->message[i] == '=')
+			equals_count++;
+		if (!ft_strchr((char *)g_base64_table, base64_data->args->message[i]) \
+		|| (base64_data->args->message[i] == '=' && i < message_length - 3))
+			return (false);
+		i++;
+	}
+	if (message_length_no_newlines % BASE64_DEC_BLOCKS != 0 || equals_count > 2)
+		return (false);
+	return (true);
 }
 
 // Main function for base64 encoding/decoding.
@@ -76,5 +133,12 @@ void	base64(t_encode_args *args)
 	if (args->encode_mode)
 		encode_message(&base64_data);
 	else if (args->decode_mode)
-		printf("Decode mode\n");
+	{
+		if (!proper_encoded_message(&base64_data))
+		{
+			errno = EBADMSG;
+			print_encode_strerror_and_exit("base64", args);
+		}
+		decode_message(&base64_data);
+	}
 }
--- srcs/incs/base64.h
+++ srcs/incs/base64.h
@@ -6,7 +6,7 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 17:55:08 by jesuserr          #+#    #+#             */
-/*   Updated: 2024/12/28 22:52:37 by jesuserr         ###   ########.fr       */
+/*   Updated: 2025/01/02 12:49:26 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 ** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
 **                              DEFINES
 */
-# define BASE64_INPUT_BLOCKS			3			// 3 * 8 bits (24 bits)
-# define BASE64_OUTPUT_BLOCKS			4			// 4 * 6 bits (24 bits)
+# define BASE64_ENC_BLOCKS				3			// 3 * 8 bits (24 bits)
+# define BASE64_DEC_BLOCKS				4			// 4 * 6 bits (24 bits)
 # define BASE64_LINE					64			// 64 characters per line
 
 /*
@@ -36,8 +36,22 @@ typedef struct s_base64_data
 	t_encode_args		*args;			// Passed as a pointer to avoid copying
 }	t_base64_data;
 
-static const uint8_t	g_base64_table[64] = \
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const uint8_t	g_base64_table[66] = \
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=\n";
+	
+static const uint8_t	g_base64_reverse_table[256] = {
+	['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3, ['E'] = 4, ['F'] = 5,
+	['G'] = 6, ['H'] = 7, ['I'] = 8, ['J'] = 9, ['K'] = 10, ['L'] = 11,
+	['M'] = 12,	['N'] = 13, ['O'] = 14, ['P'] = 15, ['Q'] = 16, ['R'] = 17,
+	['S'] = 18,	['T'] = 19, ['U'] = 20, ['V'] = 21, ['W'] = 22, ['X'] = 23,
+	['Y'] = 24,	['Z'] = 25, ['a'] = 26, ['b'] = 27, ['c'] = 28, ['d'] = 29,
+	['e'] = 30,	['f'] = 31,	['g'] = 32, ['h'] = 33, ['i'] = 34, ['j'] = 35,
+	['k'] = 36,	['l'] = 37, ['m'] = 38, ['n'] = 39,	['o'] = 40, ['p'] = 41,
+	['q'] = 42,	['r'] = 43, ['s'] = 44, ['t'] = 45, ['u'] = 46, ['v'] = 47,
+	['w'] = 48,	['x'] = 49, ['y'] = 50, ['z'] = 51, ['0'] = 52, ['1'] = 53,
+	['2'] = 54,	['3'] = 55,	['4'] = 56, ['5'] = 57, ['6'] = 58, ['7'] = 59,
+	['8'] = 60,	['9'] = 61, ['+'] = 62, ['/'] = 63
+};
 
 /*
 ** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-

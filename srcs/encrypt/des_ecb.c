5eb355bdaeea970ae80a089312ebc481853d63a5
Jesus <ivoice666@hotmail.com>
2025-02-02 13:22:20 +0100

    Encrypt and decrypt ECB (using openssl library)

--- srcs/encrypt/des_ecb.c
+++ srcs/encrypt/des_ecb.c
@@ -6,24 +6,78 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 11:18:14 by jesuserr          #+#    #+#             */
-/*   Updated: 2025/02/01 13:45:21 by jesuserr         ###   ########.fr       */
+/*   Updated: 2025/02/02 13:10:09 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/ft_ssl.h"
+#include <openssl/provider.h>
+//for OSSL_PROVIDER_load remove
+
+void	des_ecb_encrypt(t_encrypt_args *args)
+{
+	EVP_CIPHER_CTX	*ctx;
+	int				len;
+	int				ciphertext_len;
+	unsigned char	ciphertext[4096];
+	OSSL_PROVIDER	*legacy_provider;
+
+	legacy_provider = OSSL_PROVIDER_load(NULL, "legacy");
+	OSSL_PROVIDER_load(NULL, "legacy");
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_EncryptInit_ex(ctx, EVP_des_ecb(), NULL, args->hex_key, NULL);
+	EVP_EncryptUpdate(ctx, ciphertext, &len, (unsigned char *)args->message, \
+	args->message_length);
+	ciphertext_len = len;
+	EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
+	ciphertext_len += len;
+	EVP_CIPHER_CTX_free(ctx);
+	EVP_cleanup();
+	OSSL_PROVIDER_unload(legacy_provider);
+	for (int i = 0; i < ciphertext_len; i++)
+		ft_putchar_fd(ciphertext[i], args->output_fd);
+}
+
+void	des_ecb_decrypt(t_encrypt_args *args)
+{
+	EVP_CIPHER_CTX	*ctx;
+	int				len;
+	int				plaintext_len;
+	unsigned char	plaintext[4096];
+	OSSL_PROVIDER	*legacy_provider;
+
+	legacy_provider = OSSL_PROVIDER_load(NULL, "legacy");
+	OSSL_PROVIDER_load(NULL, "legacy");
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_DecryptInit_ex(ctx, EVP_des_ecb(), NULL, args->hex_key, NULL);
+	EVP_DecryptUpdate(ctx, plaintext, &len, (unsigned char *)args->message, \
+	args->message_length);
+	plaintext_len = len;
+	EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+	plaintext_len += len;
+	EVP_CIPHER_CTX_free(ctx);
+	EVP_cleanup();
+	OSSL_PROVIDER_unload(legacy_provider);
+	for (int i = 0; i < plaintext_len; i++)
+		ft_putchar_fd(plaintext[i], args->output_fd);
+}
 
 // Main function for des-ecb encryption/decryption.
 //ft_hex_dump(args, sizeof(t_encrypt_args), 8);
 void	des_ecb(t_encrypt_args *args)
 {
+	if (!args->key_provided)
+		generate_derived_key(args);
+	else
+		convert_str_to_hex(args->key, args->hex_key);
 	if (args->encrypt_mode)
 	{
 		ft_printf("Encrypting with DES-ECB\n");
-		if (!args->key_provided)
-			generate_derived_key(args);
-		else
-			convert_str_to_hex(args->key, args->hex_key);
+		des_ecb_encrypt(args);
 	}
 	else if (args->decrypt_mode)
+	{
 		ft_printf("Decrypting with DES-ECB\n");
+		des_ecb_decrypt(args);
+	}
 }
